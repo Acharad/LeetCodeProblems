f8708777ed6375a5baff7528ef506bc5f7322093
Ahmet İmran Kavraş <59830158+Acharad@users.noreply.github.com>
2022-04-03 23:59:34 +0300

    Time: 270 ms (74.15%), Space: 48.4 MB (32.92%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cs
@@ -0,0 +1,15 @@
+public class Solution 
+{
+    public void ReverseString(char[] s) 
+    {
+        char a;
+        
+        for(int i = 0; i < (s.Length)/2; i++)
+        {
+            a = s[i];
+            s[i] = s[s.Length - i - 1];
+            s[s.Length - i - 1] = a;
+        }
+        
+    }
+}
