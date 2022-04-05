2d1877141d3b6918ef9556d1cea13ccc39eb4997
Ahmet İmran Kavraş <59830158+Acharad@users.noreply.github.com>
2022-04-06 00:07:50 +0300

    Time: 189 ms (47.74%), Space: 41.3 MB (52.78%) - LeetHub

--- 27-remove-element/27-remove-element.cs
+++ 27-remove-element/27-remove-element.cs
@@ -2,7 +2,6 @@ public class Solution
 {
     public int RemoveElement(int[] nums, int val) 
     {
-        int counter = 0;
         int a = 0;
         for(int i = 0; i < nums.Length; i++)
         {
