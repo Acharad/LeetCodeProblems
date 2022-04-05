197183fdea46ce3ba74a91ba26c2f885d54146d5
Ahmet İmran Kavraş <59830158+Acharad@users.noreply.github.com>
2022-04-06 00:13:22 +0300

    Time: 215 ms (41.36%), Space: 44.5 MB (95.53%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
@@ -3,8 +3,6 @@ public class Solution
     public int RemoveDuplicates(int[] nums) 
     {
         int k = 0;
-        //[0,0,1,1,1,2,2,3,3,4] - [1,1,2]
-        // ilk indexi incelemeye gerek yok.Çünkü veirlen nums dizisi zaten sıralanmış olarak verildiği için ilk eleman kaçtan başlarsa başlasın doğru yerde olmuş olacak.
         for(int i = 1; i < nums.Length; i++)
         {
             if(nums[k] != nums[i])
