14fb9638c3c8284c7c5cfa2249e42c721065325d
Ahmet İmran Kavraş <59830158+Acharad@users.noreply.github.com>
2022-05-03 03:25:01 +0300

    Time: 506 ms (5.23%), Space: 47.2 MB (52.24%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -2,13 +2,7 @@ public class Solution
 {
     public int MaxProfit(int[] prices) 
     {
-        // almak b, satmak s
-        // alma olmadan satma olamaz o yüzden if else if olcak
-        // aldıktan sonra daha düşük bir alma noktası gelmeyene kadar price hesaplanacak
-        // daha yüksek bir price gelmeyene kadar eski price geçerli olacak.
-        
-        // gerekli olan değerler b, currentprice, maxprice.
-        int b = Int32.MaxValue, cPrice = 0, maxPrice;
+        int b = Int32.MaxValue, cPrice = 0;
 
         for(int i = 0;i < prices.Length; i++)
         {
