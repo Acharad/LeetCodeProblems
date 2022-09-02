50d1e895f80c2471a70e070c8fbd4c6f96af5ed9
Ahmet İmran Kavraş <59830158+Acharad@users.noreply.github.com>
2022-09-02 09:25:54 +0300

    Time: 198 ms (57.38%), Space: 41.7 MB (57.77%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -0,0 +1,15 @@
+public class Solution 
+{
+    public int[] RunningSum(int[] nums) 
+    {
+        int sum = 0;
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        
+        return nums;
+    }
+}
